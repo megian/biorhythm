368210f02516d98d1f1bbc21b9a06116902d473d
megian <gabisoft@freesurf.ch>
2013-03-01 18:28:48 +0100

    Files of version 0.04.1 - First public version - with Debian package files

---
+++ debian/biorhythmus-default.ex
@@ -0,0 +1,10 @@
+# Defaults for biorhythmus initscript
+# sourced by /etc/init.d/biorhythmus
+# installed at /etc/default/biorhythmus by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the biorhythmus package
+#
+0 4	* * *	root	biorhythmus_maintenance
