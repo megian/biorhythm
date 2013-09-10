ca428cf31cdc11930d1ccda9087eaa823b393d05
megian <gabisoft@freesurf.ch>
2013-09-10 20:58:05 +0200

    biorhythmus.h

---
+++ src/biorhythmus.h
@@ -0,0 +1,30 @@
+/* -*-coding: utf-8;-*- */
+
+/* Biorhythmus
+ * Copyright (C) 2003-2013, Gabriel Mainberger
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gtk/gtk.h>
+#include <glib/gi18n.h>
+
+#include "biorhythmus-math.h"
+#include "biorhythmus-chart.h"
+#include "biorhythmus-fileview.h"
+#include "biorhythmus-cli.h"
+#include "biorhythmus-print.h"
+
+#define GETTEXT_PACKAGE "biorhythmus"
+#define LOCALEDIR "locale"
