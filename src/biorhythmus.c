4177211bfaf9105903678d6989b4b86888499a89
megian <gabisoft@freesurf.ch>
2013-10-07 16:27:27 +0200

    Change

--- src/biorhythmus.c
+++ src/biorhythmus.c
@@ -17,7 +17,7 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <biorhythmus.h>
+#include "biorhythmus.h"
 
 void
 biorhythmus_gui_on_file_new_activate (GtkWidget *widget, BiorhythmusFileView *file_view)
