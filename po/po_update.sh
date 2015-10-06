b5acf54a5dc158a4af4c40f3b83384e8ba5b76a6
megian <gabisoft@freesurf.ch>
2015-10-06 21:38:24 +0200

    CMake build updates

--- po/po_update.sh
+++ po/po_update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xgettext -k_ -kN_ --keyword=Q_:1g -o test.pot ../src/*.c
-#msgmerge -o zh_CN.po zh_CN.po gcrun.pot
+xgettext -k_ -kN_ --keyword=Q_:1g -o biorhythm.pot ../src/*.c
+msgmerge -o de.po de.po biorhythm.pot
